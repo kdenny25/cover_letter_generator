f448bd411ae733352ce7940195b754746e2af080
kdenny25 <kelsead25@gmail.com>
2023-08-22 06:56:39 -0400

    added resume upload and continue working with prompts.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, url_for, redirect, flash
 import os
-from scripts.gpt_prompt import gen_text
+from scripts.gpt_prompt import gen_text, obtainText
+from werkzeug.utils import secure_filename
 
 base_dir = '.'
 app = Flask(__name__,
@@ -11,14 +12,34 @@ app = Flask(__name__,
 @app.route('/', methods=['GET', 'POST'])
 def home():  # put application's code here
     if request.method == 'GET':
-        return render_template('index.html', results='')
+        form_fill = {'your_name': '',
+                     'hiring_name': '',
+                     'resume': '',
+                     'job_listing': ''}
+
+        return render_template('index.html', results='', form_fill=form_fill)
     else:
-        role = request.form.get('role')
+        your_name = request.form.get('yourName')
+        hiring_name = request.form.get('hiringName')
+        resume = request.form.get('resume')
         job_listing = request.form.get('jobListing')
+        resume_file = request.files.get("resumeFile", None)
 
-        results = gen_text('', job_listing)
+        file_path = ''
+        if resume_file.filename != '':
+            filename = secure_filename(resume_file.filename)
+            resume_file.save(filename)
+            resume_contents = obtainText(filename)
+            print('contents extracted successfully.')
 
-        return render_template('index.html', results=results)
+        results = gen_text(resume_contents, job_listing, your_name, hiring_name)
+        form_fill = {'your_name': your_name,
+                     'hiring_name': hiring_name,
+                     'resume': resume_contents,
+                     'job_listing': job_listing,
+                     'resume_file': resume_file}
+
+        return render_template('index.html', results=results, form_fill=form_fill)
 
 
 
--- scripts/gpt_prompt.py
+++ scripts/gpt_prompt.py
@@ -5,17 +5,43 @@ import os
 
 openai.api_key = OPENAI_API_KEY
 
-def gen_text(resume, job_listing):
-    response = openai.ChatCompletion.create(
+def gen_text(resume, job_listing, your_name, hiring_manager):
+    job_listing_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role":"system", "content": "You are a entry level data scientist creating cover letters."},
-            {"role":"user", "content": f"Summarize the companies objectives, qualifications and requirements from the following job listing and pull the most relevent information to be addressed in a cover letter. {job_listing}"}],
+            {"role":"user", "content": f"Summarize the companies objectives, qualifications and requirements from the "
+                                       f"following job post and it is relevent to information found in a cover letter. "
+                                       f"{job_listing}"}],
+        temperature=0,
+        max_tokens=2048
+    )
+    jl_result = job_listing_response['choices'][0]['message']['content']
+
+    background_response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {"role":"assistant", "content": f"Professionally identify relationships "
+                                            f"between items on the following resume and job listing summary. Prioritize the most relevant items first RESUME: "
+                                            f"{resume}, JOB LISTING SUMMARY: {jl_result}"}
+        ],
+        temperature=0,
+        max_tokens=2048
+    )
+    background_result = background_response['choices'][0]['message']['content']
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user",
+             "content": f"Use at most 250 words and don't be repetative. Using the following summary of a job listing and background create a "
+                        f"cover letter using a 5th "
+                        f"grade writing level. BACKGROUND: {background_result}, JOB LISTING: {jl_result}. The applicants "
+                        f"name is {your_name} and the hiring managers name is {hiring_manager}."}],
         temperature=0,
         max_tokens=2048
     )
     result = response['choices'][0]['message']['content']
-
     return result
 
 def obtainText(docFileName):
